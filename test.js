0548513bc34cde383f604fbe7a348f82c2096d89
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-08-29 03:06:55 +0000

    jest tests

---
+++ test.js
@@ -0,0 +1,26 @@
+// import the function sum from the app.js file
+const { sum } = require('./app.js');
+const { fromEuroToDollar } = require('./app.js');
+const { fromDollarToYen } = require('./app.js');
+const { fromYenToPound } = require('./app.js')
+
+
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum(14, 9);
+    expect(total).toBe(23);
+ });
+
+test("One euro should be 1.2 dollars", () => {
+     let dollar = fromEuroToDollar(3.5)
+     expect(dollar).toBe(4.2); 
+});
+
+test("One dollar should be 106.58 yens", () => {
+    let yen = fromDollarToYen(3.5)
+    expect(yen).toBe(373.04);
+});
+
+test("One yen should be 0.01 pounds", () => {
+    let pound = fromYenToPound(5000)
+    expect(pound).toBe(31.27);
+});
